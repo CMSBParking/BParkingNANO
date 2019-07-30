f7f3fda91a4d25758e2f129c35ab8f03f08e3ebf
arabella <arabella.martelli@cern.ch>
2019-07-30 14:30:05 +0100

    muon table for BPark

---
+++ BParkingNano/python/muonsBPark_cff.py
@@ -0,0 +1,68 @@
+import FWCore.ParameterSet.Config as cms
+from PhysicsTools.NanoAOD.muons_cff import *
+
+
+muonBParkTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+    src = cms.InputTag("slimmedMuons"),
+    cut = cms.string(""), #we should not filter on cross linked collections
+    name = cms.string("Muon"),
+    doc  = cms.string("slimmedMuons for BPark after basic selection (" + finalMuonsBPark.cut.value()+")"),
+    singleton = cms.bool(False), # the number of entries is variable
+    extension = cms.bool(False), # this is the main table for the muons
+    variables = cms.PSet(CandVars,
+        ptErr   = Var("bestTrack().ptError()", float, doc = "ptError of the muon track", precision=6),
+        dz = Var("dB('PVDZ')",float,doc="dz (with sign) wrt first PV, in cm",precision=10),
+        dzErr = Var("abs(edB('PVDZ'))",float,doc="dz uncertainty, in cm",precision=6),
+        dxy = Var("dB('PV2D')",float,doc="dxy (with sign) wrt first PV, in cm",precision=10),
+        dxyErr = Var("edB('PV2D')",float,doc="dxy uncertainty, in cm",precision=6),
+        vz = Var("vz()",float,doc="z coordinate of vertex position, in cm",precision=6),
+        sip3d = Var("abs(dB('PV3D')/edB('PV3D'))",float,doc="3D impact parameter significance wrt first PV",precision=10),
+        segmentComp   = Var("segmentCompatibility()", float, doc = "muon segment compatibility", precision=14), # keep higher precision since people have cuts with 3 digits on this
+        nStations = Var("numberOfMatchedStations", int, doc = "number of matched stations with default arbitration (segment & track)"),
+        #nTrackerLayers = Var("innerTrack().hitPattern().trackerLayersWithMeasurement()", int, doc = "number of layers in the tracker"),
+        #jetIdx = Var("?hasUserCand('jet')?userCand('jet').key():-1", int, doc="index of the associated jet (-1 if none)"),
+        #miniPFRelIso_chg = Var("userFloat('miniIsoChg')/pt",float,doc="mini PF relative isolation, charged component"),
+        #miniPFRelIso_all = Var("userFloat('miniIsoAll')/pt",float,doc="mini PF relative isolation, total (with scaled rho*EA PU corrections)"),
+        pfRelIso03_chg = Var("pfIsolationR03().sumChargedHadronPt/pt",float,doc="PF relative isolation dR=0.3, charged component"),
+        pfRelIso03_all = Var("(pfIsolationR03().sumChargedHadronPt + max(pfIsolationR03().sumNeutralHadronEt + pfIsolationR03().sumPhotonEt - pfIsolationR03().sumPUPt/2,0.0))/pt",float,doc="PF relative isolation dR=0.3, total (deltaBeta corrections)"),
+        pfRelIso04_all = Var("(pfIsolationR04().sumChargedHadronPt + max(pfIsolationR04().sumNeutralHadronEt + pfIsolationR04().sumPhotonEt - pfIsolationR04().sumPUPt/2,0.0))/pt",float,doc="PF relative isolation dR=0.4, total (deltaBeta corrections)"),
+        #jetRelIso = Var("?userCand('jetForLepJetVar').isNonnull()?(1./userFloat('ptRatio'))-1.:(pfIsolationR04().sumChargedHadronPt + max(pfIsolationR04().sumNeutralHadronEt + pfIsolationR04().sumPhotonEt - pfIsolationR04().sumPUPt/2,0.0))/pt",float,doc="Relative isolation in matched jet (1/ptRatio-1, pfRelIso04_all if no matched jet)",precision=8),
+        tightCharge = Var("?(muonBestTrack().ptError()/muonBestTrack().pt() < 0.2)?2:0",int,doc="Tight charge criterion using pterr/pt of muonBestTrack (0:fail, 2:pass)"),
+        isPFcand = Var("isPFMuon",bool,doc="muon is PF candidate"),
+        isGlobal = Var("isGlobalMuon",bool,doc="muon is global muon"),
+        isTracker = Var("isTrackerMuon",bool,doc="muon is tracker muon"),
+        mediumId = Var("passed('CutBasedIdMedium')",bool,doc="cut-based ID, medium WP"),
+        mediumPromptId = Var("passed('CutBasedIdMediumPrompt')",bool,doc="cut-based ID, medium prompt WP"),
+        tightId = Var("passed('CutBasedIdTight')",bool,doc="cut-based ID, tight WP"),
+        softId = Var("passed('SoftCutBasedId')",bool,doc="soft cut-based ID"),
+        softMvaId = Var("passed('SoftMvaId')",bool,doc="soft MVA ID"),
+        highPtId = Var("?passed('CutBasedIdGlobalHighPt')?2:passed('CutBasedIdTrkHighPt')","uint8",doc="high-pT cut-based ID (1 = tracker high pT, 2 = global high pT, which includes tracker high pT)"),
+        pfIsoId = Var("passed('PFIsoVeryLoose')+passed('PFIsoLoose')+passed('PFIsoMedium')+passed('PFIsoTight')+passed('PFIsoVeryTight')+passed('PFIsoVeryVeryTight')","uint8",doc="PFIso ID from miniAOD selector (1=PFIsoVeryLoose, 2=PFIsoLoose, 3=PFIsoMedium, 4=PFIsoTight, 5=PFIsoVeryTight, 6=PFIsoVeryVeryTight)"),
+        tkIsoId = Var("?passed('TkIsoTight')?2:passed('TkIsoLoose')","uint8",doc="TkIso ID (1=TkIsoLoose, 2=TkIsoTight)"),
+        mvaId = Var("passed('MvaLoose')+passed('MvaMedium')+passed('MvaTight')","uint8",doc="Mva ID from miniAOD selector (1=MvaLoose, 2=MvaMedium, 3=MvaTight)"),
+        miniIsoId = Var("passed('MiniIsoLoose')+passed('MiniIsoMedium')+passed('MiniIsoTight')+passed('MiniIsoVeryTight')","uint8",doc="MiniIso ID from miniAOD selector (1=MiniIsoLoose, 2=MiniIsoMedium, 3=MiniIsoTight, 4=MiniIsoVeryTight)"),
+        multiIsoId = Var("?passed('MultiIsoMedium')?2:passed('MultiIsoLoose')","uint8",doc="MultiIsoId from miniAOD selector (1=MultiIsoLoose, 2=MultiIsoMedium)"),
+        triggerIdLoose = Var("passed('TriggerIdLoose')",bool,doc="TriggerIdLoose ID"),
+        inTimeMuon = Var("passed('InTimeMuon')",bool,doc="inTimeMuon ID"),
+    ),
+)
+
+
+muonsMCMatchForBParkTable = muonsMCMatchForTable.clone(
+    src         = muonBParkTable.src,                         # final reco collection
+    matched     = cms.InputTag("finalGenParticlesBPark"),     # final mc-truth particle collection
+)
+
+muonMCBParkTable = muonMCTable.clone(
+    src     = muonBParkTable.src,
+    mcMap   = cms.InputTag("muonsMCMatchForBParkTable"),
+    objName = muonBParkTable.name,
+    objType = muonBParkTable.name,
+)
+
+
+
+
+muonBParkMC = cms.Sequence(muonsMCMatchForBParkTable + muonMCBParkTable)
+muonBParkTables = cms.Sequence(muonBParkTable)
+
