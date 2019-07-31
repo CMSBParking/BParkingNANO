923de4c7f066e7a4fadc3d7b587b10ae77470266
arabella <arabella.martelli@cern.ch>
2019-07-31 14:47:01 +0100

    update nanoBPark sequence and run test

--- BParkingNano/python/nanoBPark_cff.py
+++ BParkingNano/python/nanoBPark_cff.py
@@ -1,18 +1,20 @@
 from __future__ import print_function
 import FWCore.ParameterSet.Config as cms
 from PhysicsTools.NanoAOD.common_cff import *
-from PhysicsTools.BParkingNano.muonsBPark_cff import * 
 from PhysicsTools.NanoAOD.globals_cff import *
 from PhysicsTools.NanoAOD.nano_cff import *
+from PhysicsTools.NanoAOD.vertices_cff import *
+from PhysicsTools.NanoAOD.NanoAODEDMEventContent_cff import *
+
+##for gen and trigger muon
 from PhysicsTools.BParkingNano.genparticlesBPark_cff import *
 from PhysicsTools.BParkingNano.particlelevelBPark_cff import *
-from PhysicsTools.NanoAOD.vertices_cff import *
 from PhysicsTools.BParkingNano.triggerObjectsBPark_cff import *
 from PhysicsTools.BParkingNano.RecoTrgMuonCand_cff import *
-from PhysicsTools.NanoAOD.NanoAODEDMEventContent_cff import *
-##all following only cleaned wrt trigger muon
-#need electrons pT > 1 and lowpt
-#need tracks
+
+## filtered input collections
+from PhysicsTools.BParkingNano.electronsBPark_cff import * 
+from PhysicsTools.BParkingNano.muonsBPark_cff import * ##to be replaced with slimmed muons filtered wrt trigger muon
 
 
 
@@ -20,22 +22,25 @@ from PhysicsTools.NanoAOD.NanoAODEDMEventContent_cff import *
 nanoSequenceOnlyFullSim = cms.Sequence(triggerObjectBParkTables + l1bits)
 
 nanoSequence = cms.Sequence(nanoMetadata + 
-                            vertexSequence + 
-                            muonBParkTables + 
+                            vertexSequence +                             
                             globalTables + vertexTables + 
                             triggerObjectBParkTables + l1bits)
 
 nanoSequenceMC = cms.Sequence(particleLevelBParkSequence + genParticleBParkSequence + 
-                              muonBParkMC + 
+                              muonBParkMC + electronBParkMC +
                               globalTablesMC + genWeightsTable + genParticleBParkTables + particleLevelBParkTables + lheInfoTable) 
 
 
 
 def nanoAOD_customizeMC(process):
-    process.nanoSequence.insert(process.nanoSequence.index(triggerObjectBParkTables), cms.Sequence(process.nanoSequenceMC))
+    process.nanoSequence = cms.Sequence( process.nanoSequence + nanoSequenceMC)
     return process
 
 def nanoAOD_customizeMuonTriggerBPark(process):
     process.nanoSequence = cms.Sequence( process.nanoSequence + RecoTrgMuonCandSequence + RecoTrgMuonCandTables)
+    return process
+
+def nanoAOD_customizeElectronFilteredBPark(process):
+    process.nanoSequence = cms.Sequence( process.nanoSequence + electronsBParkSequence + electronBParkTables)
     return process
 
