59a0ec7bea3c137a3341137bda0ec8a15c9de9af
arabella <arabella.martelli@cern.ch>
2019-08-02 10:15:02 +0200

    add isPacked isLost

--- BParkingNano/python/tracksBPark_cff.py
+++ BParkingNano/python/tracksBPark_cff.py
@@ -36,7 +36,7 @@ trackBParkTable = cms.EDProducer(
         vy = Var("daughter(0).vy()", float, doc="y coordinate of vertex position, in cm", precision=10),
         vz = Var("daughter(0).vz()", float, doc="z coordinate of vertex position, in cm", precision=10),
         isPacked = Var("userInt('isPacked')",int,doc="track from packedCandidate collection", precision=10),
-        isLost = Var("userInt('isLost')",int,doc="track from lostTrack collection", precision=10),
+        isLostTrk = Var("userInt('isLostTrk')",int,doc="track from lostTrack collection", precision=10),
         dz = Var("userFloat('dz')",float,doc="dz (with sign) wrt first PV, in cm", precision=10),
         dxy = Var("userFloat('dxy')",float,doc="dxy (with sign) wrt first PV, in cm", precision=10),
         dzS = Var("userFloat('dzS')", float, doc="dz/err (with sign) wrt first PV, in cm", precision=10),
