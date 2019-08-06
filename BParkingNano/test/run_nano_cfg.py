4ecf4353fef14681acab99a75fbd506b99318ac7
mauro verzetti <mverzett@cern.ch>
2019-08-06 15:58:44 +0200

    moved test cfg to main cfg, added VarParsing to ease switches usage

--- BParkingNano/test/run_nano_cfg.py
+++ BParkingNano/test/run_nano_cfg.py
@@ -1,14 +1,19 @@
-import FWCore.ParameterSet.Config as cms
 from FWCore.ParameterSet.VarParsing import VarParsing
+import FWCore.ParameterSet.Config as cms
 
 options = VarParsing('python')
 
-options.register('isData', True,
+options.register('isMC', False,
     VarParsing.multiplicity.singleton,
     VarParsing.varType.bool,
     "Run this on real data"
 )
 options.register('wantSummary', True,
+    VarParsing.multiplicity.singleton,
+    VarParsing.varType.bool,
+    "Run this on real data"
+)
+options.register('wantFullRECO', False,
     VarParsing.multiplicity.singleton,
     VarParsing.varType.bool,
     "Run this on real data"
@@ -19,82 +24,112 @@ options.register('reportEvery', 1000,
     "report every N events"
 )
 
-options.setDefault('maxEvents', 100)
+options.setDefault('maxEvents', 1000)
+options.setDefault('tag', '10215')
 options.setDefault('inputFiles', ['/store/data/Run2018B/ParkingBPH4/MINIAOD/05May2019-v2/230000/6B5A24B1-0E6E-504B-8331-BD899EB60110.root'])
-options.setDefault('outputFile', 'bparking_nano.root')
 options.parseArguments()
 
-globaltag = '102X_dataRun2_Sep2018Rereco_v1' if options.isData else'102X_upgrade2018_realistic_v15' 
+globaltag = '102X_dataRun2_Sep2018Rereco_v1' if options.isMC else '102X_upgrade2018_realistic_v15'
+extension = {False : 'data', True : 'mc'}
+outputFileNANO = cms.untracked.string('_'.join(['testBParkNANO', extension[options.isMC], options.tag])+'.root')
+outputFileFEVT = cms.untracked.string('_'.join(['testBParkFullEvt', extension[options.isMC], options.tag])+'.root')
+if options.isMC and not options._beenSet['inputFiles']:
+    options.inputFiles.pop()
+    options.inputFiles = ['/store/user/bainbrid/lowpteleid/BuToKJpsi_Toee_MuFilter_SoftQCDnonD_TuneCP5_13TeV-pythia8-evtgen/crab_lowpteleid/190328_152903/0000/step3_inMINIAODSIM_99.root']
+annotation = '%s nevts:%d' % (outputFileNANO, options.maxEvents)
 
-process = cms.Process('ParkingNano')
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process('BParkNANO',eras.Run2_2018)
 
-process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
-process.load("Configuration.StandardSequences.MagneticField_cff")
-process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('PhysicsTools.BParkingNano.nanoBPark_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
 process.MessageLogger.cerr.FwkReport.reportEvery = options.reportEvery
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(options.maxEvents)
+)
 
-process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, globaltag, '')
-
-process.maxEvents = cms.untracked.PSet( 
-   input = cms.untracked.int32(options.maxEvents) 
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(options.inputFiles),
+    secondaryFileNames = cms.untracked.vstring()
 )
 
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(options.wantSummary),
 )
 
-process.source = cms.Source(
-   "PoolSource",
-   # replace 'myfile.root' with the source file you want to use
-   fileNames = cms.untracked.vstring(
-      options.inputFiles
-   ),
-   secondaryFileNames=cms.untracked.vstring(
-   ),
-   inputCommands=cms.untracked.vstring(
-      'keep *',
-      'drop *_ctppsPixelClusters_*_*',      
-   ),
+process.nanoMetadata.strings.tag = annotation
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string(annotation),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
 )
 
-#
-# Here goes the ntuplising code with selection stuff
-#
-process.ntuplesSeq = cms.Sequence(
-)
-
-process.p = cms.Path(
-   process.ntuplesSeq
-   )
-
-#
-# NanoAOD output module
-#
-from PhysicsTools.NanoAOD.NanoAODEDMEventContent_cff import NANOAODEventContent
-process.NANOAODoutput = cms.OutputModule(
-   "NanoAODOutputModule",
-   compressionAlgorithm = cms.untracked.string('LZMA'),
-   compressionLevel = cms.untracked.int32(9),
-   SelectEvents = cms.untracked.PSet(
-      SelectEvents = cms.vstring('p') # Select only events passing path p
-      ),
-   dataset = cms.untracked.PSet(
-      dataTier = cms.untracked.string('NANOAOD'),
+# Output definition
+process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-RECO'),
         filterName = cms.untracked.string('')
-   ),
-   fileName = cms.untracked.string(options.outputFile),
-   outputCommands = cms.untracked.vstring(
-      'drop *',
-      "keep nanoaodFlatTable_*Table_*_*",     # event data
-      "keep nanoaodUniqueString_nanoMetadata_*_*",   # basic metadata
-   ),
+    ),
+    fileName = outputFileFEVT,
+    outputCommands = (cms.untracked.vstring('keep *')),
+    splitLevel = cms.untracked.int32(0)
 )
 
-process.endjob = cms.EndPath(
-   process.NANOAODoutput
+process.NANOAODoutput = cms.OutputModule("NanoAODOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('NANOAOD'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = outputFileNANO,
+    outputCommands = process.NANOAODEventContent.outputCommands
 )
 
-process.schedule = cms.Schedule(process.p, process.endjob)   
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, globaltag, '')
+
+
+# Path and EndPath definitions
+process.nanoAOD_step = cms.Path(process.nanoSequence)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
+process.NANOAODoutput_step = cms.EndPath(process.NANOAODoutput)
+
+
+# Schedule definition
+process.schedule = cms.Schedule(process.nanoAOD_step,process.endjob_step, process.NANOAODoutput_step)
+if options.wantFullRECO:
+    process.schedule = cms.Schedule(process.nanoAOD_step,process.endjob_step, process.FEVTDEBUGHLToutput_step, process.NANOAODoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+from PhysicsTools.BParkingNano.nanoBPark_cff import *
+process = nanoAOD_customizeMuonTriggerBPark(process)
+process = nanoAOD_customizeElectronFilteredBPark(process)
+process = nanoAOD_customizeTrackFilteredBPark(process)
+
+# customisation of the process.
+if options.isMC:
+    from PhysicsTools.BParkingNano.nanoBPark_cff import nanoAOD_customizeMC
+    process = nanoAOD_customizeMC(process)
+
+
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
