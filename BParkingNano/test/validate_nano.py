ac8490ba31fd3eb707754d67d9697246d51b04a0
mauro verzetti <mverzett@cern.ch>
2019-08-07 10:40:02 +0200

    many fixes to the validation

--- BParkingNano/test/validate_nano.py
+++ BParkingNano/test/validate_nano.py
@@ -40,29 +40,45 @@ legacy_mapping = { #mapping between George's Ntuples and Nano
   'el_islowpt' : 'Electron_isLowPt',
   # TODO: Add variables as they are validated and produced
 }
+# invert map as well
+legacy_inverted = {v : k for k, v in legacy_mapping.iteritems()}
 
-def load_nano(infile, legacy = False):
-  uf = uproot.open(infile)
-  tt = uf['demo/mytree'] if legacy else uf['Events']
-  arrs = tt.arrays(tt.keys())
-  if legacy:
-    return {j : arrs[i] for i, j in legacy_mapping.iteritems()}
-  return arrs
+class NanoFrame(object):
+  def __init__(self, infile, legacy = False):
+    self.uf = uproot.open(infile)
+    self.tt = self.uf['demo/mytree'] if legacy else self.uf['Events']
+    self.legacy = legacy
+
+  def __getitem__(self, key):
+    return self.tt.array(legacy_inverted[key] if self.legacy else key)
+
+  def keys(self):
+    return legacy_mapping.keys() if self.legacy else self.tt.keys()
 
 def byval_validation(v1, v2):
+  if np.isinf(v2).any() or np.isnan(v2).any():
+    v1 = v1[np.invert(np.isinf(v1) | np.isnan(v1).any())]
+    v2 = v2[np.invert(np.isinf(v2) | np.isnan(v2).any())]
+
   try:
-    if v1.dtype == 'bool':
-      return v1 ^ v2
+    if v1.dtype == 'bool' or np.issubdtype(v1.dtype, np.integer):
+      return (v1 == v2).all()
     else:
       return ((np.abs(v1 - v2) / (abs(v1) + eps)) < 0.001).all()
   except ValueError:
     return False
 
 def stat_validation(v1, v2, name = '', nbins = 20):
+  if np.isinf(v2).any() or np.isnan(v2).any():
+    print '\033[1;35m', name, '--> CONTAINS INFs/NANs!\033[0m'
+    v1 = v1[np.invert(np.isinf(v1) | np.isnan(v1).any())]
+    v2 = v2[np.invert(np.isinf(v2) | np.isnan(v2).any())]
+
   if v1.shape[0] == 0 and v2.shape[0] == 0:
     return True
   elif v1.shape[0] == 0 or v2.shape[0] == 0:
     return False
+
   M = max(v1.max(), v2.max())
   m = min(v1.min(), v2.min())
   m = m * 0.9 if m > 0 else m * 1.2
@@ -70,47 +86,31 @@ def stat_validation(v1, v2, name = '', nbins = 20):
   if 'int' in str(v1.dtype):
     m = int(m) - 1
     M = int(M) + 1
-    nbins = M - m #min(M - m, nbins)
+    nbins = min(M - m, nbins*2)
   plt.clf()
-  #set_trace()
   h1, _, _ = plt.hist(v1, range = (m,M), bins = nbins, label = 'old', histtype = 'step')
   h2, _, _ = plt.hist(v2, range = (m,M), bins = nbins, label = 'new', histtype = 'step')
   plt.legend(loc='best')
   plt.savefig('validation/%s.png' % name)
+  plt.clf()
   return (h1 == h2).all()
 
-old = load_nano(args.f_old, args.legacy)
-new = load_nano(args.f_new)
-
-old_k = set(old.keys())
-new_k = set(new.keys())
-intersection = old_k.intersection(new_k)
-
-for branch in intersection:
-  v_old = old[branch]
-  v_new = new[branch]
-  if hasattr(v_old, 'flatten'):
-    v_old = v_old.flatten()
-    v_new = v_new.flatten()
-  stat_valid = stat_validation(v_old, v_new, branch)
-  val_valid  = byval_validation(v_old, v_new)
-  if val_valid and stat_valid:
-    print '\033[1;32m', branch, '--> OK!\033[0m'
-  elif stat_valid:
-    print '\033[1;35m', branch, '--> FAILS BY VALUE CHECK ONLY!\033[0m'
-  else:
-    print '\033[1;31m', branch, '--> FAILS ALL CHECKS!\033[0m'
+old = NanoFrame(args.f_old, args.legacy)
+new = NanoFrame(args.f_new)
 
 #
 # Size Checks
 #
-uf = uproot.open(args.f_new)
-tt = uf['Events']
+uf = new.uf
+tt = new.tt
 
 branches_and_size = {i.name : i.compressedbytes() for i in tt.allvalues()}
 tot_branches = sum(branches_and_size.values())
 n_entries = len(tt)
-n_processed = int(uf['tag'].split('nevts:')[1])
+try:
+  n_processed = int(uf['tag'].split('nevts:')[1])
+except:
+  n_processed = -1
 
 from collections import defaultdict
 groups = defaultdict(long)
@@ -146,5 +146,29 @@ leg = plt.legend(
     bbox_to_anchor = (0.95, 0.5),
     mode="expand", borderaxespad=0., frameon=False
 )
-plt.title('Total size: %.3f kB / evt (%d events / %d processed)' % (tot_branches/(10.**3 * n_entries), n_entries, n_processed))
+title = 'Total size: %.3f kB / evt (%d events / %d processed)' % (tot_branches/(10.**3 * n_entries), n_entries, n_processed)
+print title
+plt.title(title)
 fig.savefig('validation/size.png')
+
+#
+# Branch checks
+#
+old_k = set(old.keys())
+new_k = set(new.keys())
+intersection = old_k.intersection(new_k)
+
+for branch in intersection:
+  v_old = old[branch]
+  v_new = new[branch]
+  if hasattr(v_old, 'flatten'):
+    v_old = v_old.flatten()
+    v_new = v_new.flatten()
+  stat_valid = stat_validation(v_old, v_new, branch)
+  val_valid  = byval_validation(v_old, v_new)
+  if val_valid and stat_valid:
+    print '\033[1;32m', branch, '--> OK!\033[0m'
+  elif stat_valid:
+    print '\033[1;35m', branch, '--> FAILS BY VALUE CHECK ONLY!\033[0m'
+  else:
+    print '\033[1;31m', branch, '--> FAILS ALL CHECKS!\033[0m'
