4c4bd6deafe45cc6309cd90780a4a0eda3e3486a
mauro verzetti <mverzett@cern.ch>
2019-08-07 13:42:36 +0200

    simple script to analyse the framework report and extract timing info

---
+++ BParkingNano/test/time_analysis.py
@@ -0,0 +1,74 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os
+from pdb import set_trace
+eps = 10**-7
+from argparse import ArgumentParser
+import numpy as np
+
+parser = ArgumentParser()
+parser.add_argument('inval', help='file path')
+args = parser.parse_args()
+
+#Module Summary
+infile = open(args.inval).read()
+modules = infile.split('TimeReport ---------- Module Summary ---[Real sec]----')[1].split('T---Report end!')[0]
+time_rep = infile.split('TimeReport ---------- Event  Summary')[1].split('\n\n')[0].split('\n')
+
+module_times = []
+for l in modules.split('\n'):
+  line = l.strip()
+  if not line: continue
+  if line.endswith('Name'): continue
+  info = line.split()
+  module_times.append((info[-1], float(info[1])))
+
+tot_time = sum(i for _, i in module_times)
+from collections import defaultdict
+groups = defaultdict(float)
+
+for name, time in module_times:
+  if 'Table' in name:
+    groups['Tables'] += time
+  elif 'electron' in name.lower():
+    groups['Electrons'] += time
+  elif 'track' in name.lower():
+    groups['Tracks'] += time
+  elif 'muon' in name.lower():
+    groups['Muons'] += time
+  elif name.endswith('output'):
+    groups['I/O'] += time
+  else:
+    groups['Other'] += time
+
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+cm = plt.get_cmap('rainbow')
+cNorm  = colors.Normalize(vmin=0, vmax=len(groups)-1)
+scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+cols = [scalarMap.to_rgba(i) for i in range(len(groups))]
+
+def writer(pct):
+  if pct < 5: return ''
+  else: return '%.1f%%' % pct
+
+plt.clf()
+fig = plt.figure(
+    figsize=(12, 6), 
+)
+plt.subplot(1, 2, 1)
+vals = np.array(groups.values())/tot_time
+wedges = plt.pie(vals, autopct = writer, colors = cols)
+names = ['%s (%.1f%%)' % (n, float(p)*100/tot_time) for n, p in groups.iteritems()]
+leg = plt.legend(
+    wedges[0], names, loc = 5,
+    bbox_to_anchor = (0.95, 0.5),
+    mode="expand", borderaxespad=0., frameon=False
+)
+cpu = float(time_rep[1].split(' = ')[1])
+wall = float(time_rep[2].split(' = ')[1])
+
+title = 'Execution time: %.3f [s] (CPU / evt), %.3f [s] (Wall / evt)' % (cpu, wall)
+plt.title(title)
+fig.savefig('validation/timing.png')
