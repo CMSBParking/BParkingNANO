a026c086d40874977561ae273f5a8903f5e80c1d
sara <sara.fiorendi@cern.ch>
2019-09-11 16:06:52 +0200

    remove making rivetLeptonTable

--- BParkingNano/python/particlelevelBPark_cff.py
+++ BParkingNano/python/particlelevelBPark_cff.py
@@ -1,6 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 from  PhysicsTools.NanoAOD.particlelevel_cff import *
 
-
 particleLevelBParkSequence = cms.Sequence(mergedGenParticles + genParticles2HepMC + particleLevel)
-particleLevelBParkTables = cms.Sequence(rivetLeptonTable)
